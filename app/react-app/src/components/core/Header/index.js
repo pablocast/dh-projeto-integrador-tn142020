23e76fceb0cdf79f23c478355b28b683c77586d7
Pablo Castano <depablocastano@gmail.com>
2021-03-15 19:38:33 -0300

    adicionando header com material ui

--- app/react-app/src/components/core/Header/index.js
+++ app/react-app/src/components/core/Header/index.js
@@ -1,11 +1,28 @@
 import './style.css'
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const Header = () => {
   return (
-    <section className="header">
-        <p>This is header</p>
-    </section>
-  )
+    <div >
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Header
+export default Header; 
