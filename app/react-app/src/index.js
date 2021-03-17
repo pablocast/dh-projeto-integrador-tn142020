6d4a876b1ba144e6924e91d23822654f07ec498b
bruno simplicio <bruno_2105@outlook.com>
2021-03-17 17:09:18 -0300

    feature: skill-container

--- app/react-app/src/index.js
+++ app/react-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<script href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
 
 ReactDOM.render(
   <React.StrictMode>
