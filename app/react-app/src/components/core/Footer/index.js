113cf01e675ff0ee438bc6782cd7354f5ade6ed9
bruno simplicio <bruno_2105@outlook.com>
2021-03-18 10:47:20 -0300

    feature: footer-react

--- app/react-app/src/components/core/Footer/index.js
+++ app/react-app/src/components/core/Footer/index.js
@@ -3,9 +3,31 @@ import './style.css'
 const Footer = () => {
 
     return (
-        <section className='footer'> 
-            <p>This is footer</p>
-        </section>
+        <footer class="footer">
+
+        <div class="footer-copy">
+          <p>Todos os direitos reservados BrainXpert &copy;</p>
+        </div>
+        <div class="footer-container-links">
+          <ul>
+            <li class="titulo_rodape">Cursos</li>
+            <li class="font-light">Front-end</li>
+            <li class="font-light">Back-End</li>
+            <li class="font-light">Mobile-Futter</li>
+            <li class="font-light">Full-Stack</li>
+          </ul>
+        </div>
+        <div class="footer-container-links">
+          <ul>
+            <li class="titulo_rodape">Empresas</li>
+            <li class="font-light">Banco Itai</li>
+            <li class="font-light">Magazine Luci</li>
+            <li class="font-light">Nicolas e Cia</li>
+            <li class="font-light">Summer-down</li>
+          </ul>
+        </div>
+    
+      </footer>
     )
 
 }
