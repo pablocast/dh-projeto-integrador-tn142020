e5d78e7c372ad1f66009735619f2ac6e5f0e1f76
Pablo Castano <depablocastano@gmail.com>
2021-03-18 09:16:12 -0300

    adicionando componente 4

--- app/react-app/src/components/core/Componente-2/index.js
+++ app/react-app/src/components/core/Componente-2/index.js
@@ -3,9 +3,7 @@ import './style.css'
 const Component2 = () => {
 
     return (
-        <div className="comp-2">
-            <p>This is comp2</p>
-        </div>
+      <p>This is comp2</p>
     )
 
 }
