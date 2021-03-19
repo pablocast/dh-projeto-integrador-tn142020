def964959d0d07b27a18c0527cfde91ec1f38cac
bruno simplicio <bruno_2105@outlook.com>
2021-03-19 19:11:42 -0300

    feature: criando pageCursos

---
+++ app/react-app/src/components/HeaderPageCursos/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+import imageLogo from '../../assets/logo.png'
+
+const HeaderPagesCursos = () => {
+    <header>
+        <img src={imageLogo} alt='logo brainXpert' />
+    </header>
+}
+
+export default HeaderPagesCursos
