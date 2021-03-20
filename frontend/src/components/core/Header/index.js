9d4502a5db7a0209c333904db167204c9cc38ddd
Pablo Castano <depablocastano@gmail.com>
2021-03-20 00:05:00 -0300

    adicionando hover botao cadastro

--- frontend/src/components/core/Header/index.js
+++ frontend/src/components/core/Header/index.js
@@ -1,19 +1,34 @@
 import './style.css'
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../../assets/img/logo.jpg'
 
 const Header = () => {
+  const [Cadastro, setCadastro] = useState(false)
+
   return (
     <header className="Header">
       <img src={logo} className='logo' alt='logo' />
-      <nav className='Nav'>
+      <nav className='Nav' >
         <a href='/plataforma'>A Plataforma</a>
         <a href='/cursos'>Cursos</a>
         <a href='/empresas'>Empresas</a>
         <a href='/contato'>Contato</a>
-        <a href='/cadastro' id="Cadastro">Cadastre-se</a>
+        <div className='Cadastro' onMouseOver={() => setCadastro(true)} >
+          <a href='' className='Cadastro_button' >Cadastre-se</a>
+        </div>
       </nav>
-    </header>
+      {
+        Cadastro
+          ? (
+            <div className='Cadastro__menu' onMouseLeave={() => setCadastro(false)} >
+              <a href="/estudantes/sign_in"> Sou estudante </a>
+              <a href="/empresa/sign_in"> Sou empresa </a>
+            </div>
+          ) : (
+            null
+          )
+      }
+    </header >
   );
 }
 
