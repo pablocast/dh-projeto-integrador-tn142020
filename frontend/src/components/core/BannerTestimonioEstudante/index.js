ee69478a22d154d1a96d35445af2cea8a04a8fed
Pablo Castano <depablocastano@gmail.com>
2021-03-25 17:25:09 -0300

    agregando banner testimunhas

--- frontend/src/components/core/BannerTestimonioEstudante/index.js
+++ frontend/src/components/core/BannerTestimonioEstudante/index.js
@@ -1,11 +1,55 @@
 import './style.css'
+import { Row, Col, Container } from 'react-bootstrap'
+import instagram from '../../../assets/img/instagram-white.png'
+import facebook from '../../../assets/img/facebook-white.png'
+import google from '../../../assets/img/google-white.png'
+import amazon from '../../../assets/img/amazon-white.png'
+import uber from '../../../assets/img/uber-white.png'
 
-const BannerRegistrarEstudante = () => {
+const BannerTestimonioEstudante = () => {
 
     return (
-        <p>This is two</p>
+        <section >
+            <Container className="testimonials">
+                <Row>
+                    <Col lg={{ span: 12 }}>
+                        <p className="companies-text">Onde as melhores empresas contrataram desenvolvedores</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={{ span: 12 }} className='companies-logo'>
+                        <div className='logo-wrapper'>
+                            <img src={instagram}/>
+                        </div>
+                        <div className='logo-wrapper'>
+                            <img src={facebook}/>
+                        </div>
+                        <div className='logo-wrapper'>
+                            <img src={google}/>
+                        </div>
+                        <div className='logo-wrapper'>
+                            <img src={amazon}/>
+                        </div>
+                        <div className='logo-wrapper'>
+                            <img src={uber}/>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg={{ span: 12 }}>
+
+
+
+                    </Col>
+                </Row>
+                <Row>
+                    <p>This is testimonials</p>
+                </Row>
+
+            </Container>
+        </section>
     )
 
 }
 
-export default BannerRegistrarEstudante
+export default BannerTestimonioEstudante
--- frontend/src/pages/NovoEstudante/index.js
+++ frontend/src/pages/NovoEstudante/index.js
@@ -3,12 +3,15 @@ import BannerPropostaEstudante from '../../components/core/BannerPropostaEstudan
 import BannerPasosEstudante from '../../components/core/BannerPasosEstudante'
 import BannerTestimonioEstudante from '../../components/core/BannerTestimonioEstudante'
 import BannerRegistrarEstudante from '../../components/core/BannerRegistrarEstudante'
+import HeaderCadastro from '../../components/core/HeaderCadastro'
 import Footer from '../../components/core/Footer'
+import logo from '../../assets/img/logo_white.png'
 
 const NovoEstudante = () => {
 
     return (
-        <>
+        <body>
+        <HeaderCadastro className='Header' myLogo={logo} />
         <div className='Registration'>
             <BannerPropostaEstudante />
             <BannerPasosEstudante />
@@ -16,7 +19,7 @@ const NovoEstudante = () => {
             <BannerRegistrarEstudante />
         </div>
         <Footer />
-        </>
+        </body>
 
     )
 }
