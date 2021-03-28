ca792cb97d2d38f20dbed11e2bb6dfff69dd43cc
souz-dev <souz.developer@gmail.com>
2021-03-28 13:02:45 -0300

    New model table

--- frontend/src/components/core/ContainerEstudantesDestaques/index.js
+++ frontend/src/components/core/ContainerEstudantesDestaques/index.js
@@ -24,7 +24,31 @@ const EstudantesDestaques = () => {
                 <tbody>
                     <tr>
                       <td>01</td>
-                      <td><div><img src="" alt="Fulano"/>
+                      <td><div><img src="https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png" alt="Fulano"/>
+                      <h4>Fulano</h4>
+                      </div></td>
+                      <td> Node React css</td>
+                      <td>Fulano</td>
+                    </tr>
+                    <tr>
+                      <td>02</td>
+                      <td><div><img src="https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png" alt="Fulano"/>
+                      <h4>Fulano</h4>
+                      </div></td>
+                      <td> Node React css</td>
+                      <td>Fulano</td>
+                    </tr>
+                    <tr>
+                      <td>03</td>
+                      <td><div><img src="https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png" alt="Fulano"/>
+                      <h4>Fulano</h4>
+                      </div></td>
+                      <td> Node React css</td>
+                      <td>Fulano</td>
+                    </tr>
+                    <tr>
+                      <td>04</td>
+                      <td><div><img src="https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png" alt="Fulano"/>
                       <h4>Fulano</h4>
                       </div></td>
                       <td> Node React css</td>
