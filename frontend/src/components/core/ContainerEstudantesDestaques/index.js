f847b22a4978be3c663644073b553d47e24d2e4f
souz-dev <souz.developer@gmail.com>
2021-03-28 12:31:32 -0300

    New model table

--- frontend/src/components/core/ContainerEstudantesDestaques/index.js
+++ frontend/src/components/core/ContainerEstudantesDestaques/index.js
@@ -9,45 +9,29 @@ const EstudantesDestaques = () => {
   return (
     <main>
       <section className="estudant-section">
-        <div className="senioridade">
-          <div>
-            <h2>Senior</h2>
-          </div>
-          <div>
-            <h2>Pleno</h2>
-          </div>
-          <div>
-            <h2>Junior</h2>
-          </div>
-          <div>
-            <h2>Estagiário</h2>
-          </div>
-        </div>
-        <div className="status-section">
-            <div className="ranck">Rank</div>
-            <div className="tier">Senioridade</div>
-            <div className="avatar">
-            <div className="aluno">Aluno(a)</div>
-            </div>
-            <div className="nome"></div>
-            <div className="container-skils">
-            <div className="skils">Skils</div>
-            </div>
-            <div className="portfolio">Portfolio</div>
-        </div>
-        <div className="aluno-sec">
-        <div className="ranck">1</div>
-
-            <div className="tier">Pleno</div>
-            <div className="avatar">
-            <div className="aluno">Fulaninho</div>
-            </div>
-            <div className="nome"></div>
-            <div className="container-skils">
-            <div className="skils"></div>
-            </div>
-            <div className="portfolio">@fulanhinho</div>
-
+      <div class="table">
+            <h5>Alunos</h5>
+            <p>Lorem Ispsum</p>
+            <table id="filmesTable">
+                <thead>
+                    <tr>
+                        <th>Ranking</th>
+                        <th>Aluno</th>
+                        <th>Skils</th>
+                        <th>Potfolio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>01</td>
+                      <td><div><img src="" alt="Fulano"/>
+                      <h4>Fulano</h4>
+                      </div></td>
+                      <td> Node React css</td>
+                      <td>Fulano</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
       </section>
     </main>
