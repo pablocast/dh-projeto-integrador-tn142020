f426439e48321eb8f7a3c0711f95d11ab07989dd
bruno simplicio <bruno_2105@outlook.com>
2021-04-20 11:50:06 -0300

    feature: responsividade header

--- src/components/core/Header/index.js
+++ src/components/core/Header/index.js
@@ -5,13 +5,25 @@ import { Link } from "react-router-dom";
 
 const Header = ({ ...props }) => {
   const [Cadastro, setCadastro] = useState(false);
+  const [click, setClick] = useState(false);
   const { isLogin } = props;
-
+  
   return (
-    <header className="Header">
+    <header className="Header container-fluid">
       <Link to="/">
         <img src={logo} className="logo" alt="logo" />
       </Link>
+      <div className={click ? 'menu-section on':'menu-section'}> 
+      <div className={click ? 'menu-toggle mobile-no': 'menu-toggle'} onClick={()=> setClick(true)}>
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+      </div>
+      <div className={click ? 'menu-toggle-mobile mobile-yes' : 'menu-toggle mobile-no'} onClick={()=> setClick(false)}>
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+      </div>
       <nav className="Nav">
         <a href="/plataforma">A plataforma</a>
         <a href="/cursos">Cursos</a>
@@ -31,8 +43,14 @@ const Header = ({ ...props }) => {
           <a href="/empresa/sign_in"> Sou empresa </a>
         </div>
       ) : null}
+      </div>
     </header>
   );
 };
 
 export default Header;
+
+<script>
+
+})
+</script>
